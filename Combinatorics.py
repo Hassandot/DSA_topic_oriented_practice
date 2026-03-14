96adbcbb346a17ca569140fdf60d093808341242
Hassan Tanveer <hassant1035@gmail.com>
2026-03-14 23:34:37 +0500

    subsets

--- Combinatorics.py
+++ Combinatorics.py
@@ -18,3 +18,24 @@ def permute(self, nums: List[int]) -> List[List[int]]:
             
         backtrack([])
         return res
+
+# 78. Subsets
+# Given an integer array nums of unique elements, return all possible subsets (the power set). The solution set must not contain duplicate subsets. The subsets can be returned in any order.
+def subsets(self, nums: List[int]) -> List[List[int]]:
+        res:List[List[int]]=[]
+        used:List[bool]=[False]*len(nums)
+
+        def bt(start,current):
+            if current:
+                res.append(current)
+            for i in range(start,len(nums)):
+                if not used[i]:
+                    used[i]=True
+                    newCurr=copy.copy(current)
+                    newCurr.append(nums[i])
+                    bt(i+1,newCurr)
+                    used[i]=False
+        res.append([])
+        bt(0,[])
+        return res
+
