bf52cc83617e137e92e50cca1a41bfdef653b367
Hassan Tanveer <hassant1035@gmail.com>
2026-03-27 17:56:28 +0500

    twoSum

--- NEETCODE-150.py
+++ NEETCODE-150.py
@@ -89,3 +89,12 @@ def isAnagram(self, s: str, t: str) -> bool:
             if mpp2[key]!=value:
                 return False
         return True
+
+def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mpp={}
+        for i in range(len(nums)):
+            if target-nums[i] in mpp:
+                return [mpp[target-nums[i]],i]
+            else:
+                mpp[nums[i]]=i
+        return []
